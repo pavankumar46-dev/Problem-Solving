117cf89dd05506a77f0e4975357e40e295a25aad
pavankumar46-dev <53743626+pavankumar46-dev@users.noreply.github.com>
2025-04-30 10:58:09 +0530

    Create left_angle_num_pattern.py

---
+++ problem_solving/patterns/left_angle_num_pattern.py
@@ -0,0 +1,15 @@
+def left_angle_num_pattern(num):
+    for i in range(1, num+1):
+        for j in range(1, i):
+            print(j, end=" ")
+        print()
+            
+            
+print_num_pattern(5)
+
+# Sample Output
+'''
+1 
+1 2 
+1 2 3 
+1 2 3 4 '''
