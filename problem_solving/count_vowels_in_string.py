518704bba5f3a536bd186721bb03c0614aa1ece8
pavankumar46-dev <53743626+pavankumar46-dev@users.noreply.github.com>
2025-05-02 06:46:08 +0530

    Create count_vowels_in_string.py

---
+++ problem_solving/count_vowels_in_string.py
@@ -0,0 +1,12 @@
+def count_vowels(string):
+    count_dict = {}
+    for i in string:
+        if i in ['a', 'e', 'i', 'o', 'u']:
+            if i in count_dict:
+                count_dict[i] = count_dict[i]+1
+            else:
+                count_dict[i] = 1
+                
+    print(count_dict)
+    
+count_vowels("pavankumar")
