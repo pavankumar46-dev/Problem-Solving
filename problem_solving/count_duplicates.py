fdeb2e0bd5e17bb32ddaae53d9ecdf66ca3d6198
pavankumar46-dev <53743626+pavankumar46-dev@users.noreply.github.com>
2025-05-02 07:00:55 +0530

    Update count_duplicates.py

--- problem_solving/count_duplicates.py
+++ problem_solving/count_duplicates.py
@@ -1,14 +1,12 @@
 def count_duplicates(lst):
-  repeated_dict = {}
-  for i in range(len(lst)):
-      count = 0
-      for j in range(0, len(lst)):
-          if lst[i] == lst[j]:
-              count = count+1
-              repeated_dict[lst[i]] = count
-  print(repeated_dict)
+    count_dict = {}
+    for i in lst:
+        if i in count_dict:
+            count_dict[i] = count_dict[i]+1
+        else:
+            count_dict[i] = 1
+    filtered = {k: v for k, v in count_dict.items() if v > 1}
+    print(filtered)
     
-    
-    
-lst = [1,2,2,4,3,5,6,7,8,1]
-count_duplicates(lst)
+
+count_duplicates([1,1,1,2,3,4,5,6,8,8])
